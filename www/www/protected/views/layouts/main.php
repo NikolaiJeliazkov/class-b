b84710353b5f9cddf2665f87a3ae83f27349ad70
njeliazkov <njeliazkov@gmail.com>
2014-05-29 13:37:41 +0300

    a

--- www/www/protected/views/layouts/main.php
+++ www/www/protected/views/layouts/main.php
@@ -76,6 +76,14 @@ if(isset($this->breadcrumbs) && count($this->breadcrumbs)>0) {
 	</div><!-- footer -->
 
 </div><!-- page -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-51466807-1', 'class-b.net');
+  ga('send', 'pageview');
+</script>
 </body>
 </html>
