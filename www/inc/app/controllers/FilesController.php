a8b87df79d77227850b386ae59ad3ed647fec3ac
njeliazkov <njeliazkov@gmail.com>
2014-10-10 13:22:21 +0300

    yiibooster

---
+++ www/inc/app/controllers/FilesController.php
@@ -0,0 +1,48 @@
+<?php
+
+class FilesController extends Controller
+{
+
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+			array('allow', // allow authenticated users to access all actions
+				'users'=>array('@'),
+			),
+			array('deny',  // deny all users
+				'users'=>array('*'),
+			),
+		);
+	}
+
+	public function actions()
+	{
+		return array(
+			'fileUpload'=>array(
+				'class'=>'ext.imperavi-redactor-widget.actions.FileUpload',
+				'uploadPath'=>path::normalize($_SERVER['DOCUMENT_ROOT'])."/files",
+				'uploadUrl'=>'/files',
+				'uploadCreate'=>true,
+				'permissions'=>0755,
+			),
+			'imageUpload'=>array(
+				'class'=>'ext.imperavi-redactor-widget.actions.ImageUpload',
+				'uploadPath'=>path::normalize($_SERVER['DOCUMENT_ROOT'])."/files",
+				'uploadUrl'=>'/files',
+				'uploadCreate'=>true,
+				'permissions'=>0755,
+			),
+			'imageList'=>array(
+				'class'=>'ext.imperavi-redactor-widget.actions.ImageList',
+				'uploadPath'=>path::normalize($_SERVER['DOCUMENT_ROOT'])."/files",
+				'uploadUrl'=>'/files',
+			),
+		);
+	}
+
+}
